670e4a2d2057dd8309ffd83fc413ed667a50c000
Rogers Ramirez <rogers.ramirez2008@gmail.com>
2020-12-09 19:25:18 -0500

    Added comments functionality to piza-hunt

--- models/Pizza.js
+++ models/Pizza.js
@@ -1,4 +1,5 @@
 const { Schema, model } = require('mongoose');
+const dateFormat = require('../utils/dateFormat');
 
 const PizzaSchema = new Schema({
     pizzaName: {
@@ -8,9 +9,10 @@ const PizzaSchema = new Schema({
       type: String
     },
     createdAt: {
-      type: Date,
-      default: Date.now
-    },
+        type: Date,
+        default: Date.now,
+        get: (createdAtVal) => dateFormat(createdAtVal)
+      },
     size: {
       type: String,
       default: 'Large'
@@ -25,8 +27,9 @@ const PizzaSchema = new Schema({
   },
   {
     toJSON: {
-      virtuals: true,
-    },
+        virtuals: true,
+        getters: true
+      },
     id: false
   }
   
